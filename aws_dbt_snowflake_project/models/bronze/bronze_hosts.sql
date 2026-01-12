c6bb038fcc9db6b6c2744d1febd143c2b23467aa
Chinnmaya Palo <chinnmaya.palo@gmail.com>
2026-01-13 02:29:15 +0530

    project done

---
+++ aws_dbt_snowflake_project/models/bronze/bronze_hosts.sql
@@ -0,0 +1,14 @@
+--{%set incremental_load = 1%}
+--{%set incremental_col = 'CREATED_AT'%}
+
+{{ config (materialized = 'incremental')}}
+
+SELECT * FROM {{ source('staging','hosts') }} 
+
+--{% if incremental_load == 1 %}
+{% if is_incremental()%}
+--WHERE {{ incremental_col}} > (SELECT COALESCE(MAX({{incremental_col}}), '1900-01-01') FROM {{ this }})
+--{% endif %}
+
+    WHERE CREATED_AT > (SELECT COALESCE(MAX({{incremental_col}}), '1900-01-01') FROM {{ this }})
+{% endif %}
---
+++ aws_dbt_snowflake_project/models/silver/silver_hosts.sql
@@ -0,0 +1,18 @@
+{{config (materialized='incremental', unique_key='HOST_ID')}}
+
+SELECT 
+    HOST_ID,
+    REPLACE (HOST_NAME, ' ', '_') AS HOST_NAME,
+    HOST_SINCE as HOST_SINCE,
+    IS_SUPERHOST as IS_SUPERHOST,
+    RESPONSE_RATE as RESPONSE_RATE,
+    CASE 
+    WHEN RESPONSE_RATE > 95 THEN 'EXCELLENT'
+    WHEN RESPONSE_RATE > 80 THEN 'GOOD'
+    WHEN RESPONSE_RATE > 60 THEN 'AVERAGE'
+    ELSE 'POOR'
+    END AS RESPONSE_RATE_QUALITY,
+    CREATED_AT
+FROM 
+    {{ ref('bronze_hosts') }}   
+
